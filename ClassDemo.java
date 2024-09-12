d751dadf0d507a14a041c5e07f42b0da1df74e68
Navyasruthi-yerramsetti <151709424+Navyasruthi-yerramsetti@users.noreply.github.com>
2024-09-12 12:44:53 +0530

    Create ClassDemo.java

---
+++ ClassDemo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+ class ClassDemo
+{
+	static String rollno;
+	static double avg;
+	public static void main(String args[])
+	{
+		rollno="23K61A05J3";
+		avg=95.5;
+		System.out.println("Roll no is "+rollno+"\nAverage is "+avg);
+	}
+}
